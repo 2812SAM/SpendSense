25617196836080863e8ef4f69f7417ffc61c794d
Swayam sharma <swayam.kaushik@gmail.com>
2026-06-15 21:13:12 +0530

    added code owners

--- flutter_application_1/lib/services/voice_service.dart
+++ flutter_application_1/lib/services/voice_service.dart
@@ -33,8 +33,10 @@ class VoiceService {
       onResult: (r) {
         if (r.finalResult) result = r.recognizedWords;
       },
-      listenFor: timeout,
-      localeId: 'en_IN', // Indian English
+      listenOptions: SpeechListenOptions(
+        listenFor: timeout,
+        localeId: 'en_IN', // Indian English
+      ),
     );
 
     // Wait for result or timeout
